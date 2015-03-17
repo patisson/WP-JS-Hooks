bd6561845b1aeba4f34fbbb699ac6b9658de24a1
Chetan Chauhan <chetanchauhan1991@gmail.com>
2015-03-17 22:31:18 +0530

    Fix minor formatting issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,33 @@
 /* jshint node:true */
 
-module.exports = function(grunt) {
-	grunt.initConfig( {
-		pkg: grunt.file.readJSON('package.json'),
-		jshint : {
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
             options: {
                 jshintrc: '.jshintrc'
             },
-			src: 'src/event-manager.js',
-			gruntfile : 'Gruntfile.js'
-		},
-		uglify: {
-			options: {
-				banner: '/**\n * Module: <%= pkg.name %>\n * Props: <%= pkg.props %>\n */\n'
-			},
-			build: {
-				src: 'src/event-manager.js',
-				dest: 'build/event-manager.min.js'
-			}
-		},
-		qunit : {
-			all_tests : 'test/qunit-test.html'
-		}
-	} );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	
-	grunt.registerTask( 'default', [ 'jshint', 'uglify', 'qunit' ] );
+            src: 'src/event-manager.js',
+            gruntfile: 'Gruntfile.js'
+        },
+        uglify: {
+            options: {
+                banner: '/**\n * Module: <%= pkg.name %>\n * Props: <%= pkg.props %>\n */\n'
+            },
+            build: {
+                src: 'src/event-manager.js',
+                dest: 'build/event-manager.min.js'
+            }
+        },
+        qunit: {
+            all_tests: 'test/qunit-test.html'
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'qunit']);
 };
